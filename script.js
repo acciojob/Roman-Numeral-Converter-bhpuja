57c0114ea2ac811bb456b8c1a385b28928b3a809
bhpuja <vpuja0421@gmail.com>
2025-01-20 16:06:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,11 +2,13 @@
 function convertToRoman() {
   let num = parseInt(document.getElementById('numberInput').value);
   
+  // Check if input is valid and in the specified range (0-100000)
   if (isNaN(num) || num < 0 || num > 100000) {
     document.getElementById('result').textContent = "Please enter a number between 0 and 100000.";
     return;
   }
 
+  // Array of Roman numerals and their corresponding values
   const romanNumerals = [
     ['M', 1000],
     ['D', 500],
@@ -19,6 +21,7 @@ function convertToRoman() {
 
   let result = '';
   
+  // Loop through the romanNumerals array and subtract values from the number
   for (let i = 0; i < romanNumerals.length; i++) {
     while (num >= romanNumerals[i][1]) {
       result += romanNumerals[i][0];
@@ -26,5 +29,6 @@ function convertToRoman() {
     }
   }
 
+  // Display the result
   document.getElementById('result').textContent = result;
 }
